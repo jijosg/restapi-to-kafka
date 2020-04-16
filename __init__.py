10d8e31e4a0ec26a3a957bc9325ffff35eecd1f7
Jijo Sunny <1934965+jijosg@users.noreply.github.com>
2020-04-16 10:32:32 +0530

    Updated posttokafka method to post to kafka topic
    Get the body of post method and send it to a kafka topic

--- __init__.py
+++ __init__.py
@@ -1,34 +1,23 @@
+from flask import Flask,request
+from kafka import KafkaProducer
+import json
 import os
 
-from flask import Flask,request
-import json
-from flask import flash
+def create_app():
+    # create and configure the app
+    app = Flask(__name__)
 
-def create_app(test_config=None):
-    #create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE = os.path.join(app.instance_path,'flaskr.sqlite')
-    )
-
-    # A simple page that says hello
+    # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
 
-    
-    @app.route('/posttokafka',methods=["GET","POST"])
+    @app.route('/posttokafka',methods=["POST"])
     def posttokafka():
-        if request.method == "POST":
-            note = request.json
-            return note
+        note = request.json
+        #print(note)
+        producer = producer = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda m: json.dumps(m).encode('utf-8'))
+        producer.send('quick_start',value=note)
+        return 'OK'
 
-        return OK
-    
-    #verify the data you are posting by hitting this api
-    @app.route('/example')
-    def example():
-        return {'request data': request.data}
-    
     return app
